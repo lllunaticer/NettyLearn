cd2067da02330eb0383365db4b8591228769777b
longxingjian <longxingjian@kuaishou.com>
2023-05-11 21:22:14 +0800

    start netty

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # NettyLearn
+来源[Netty 即时聊天实战与底层原理](https://weread.qq.com/web/reader/d3032a20813ab6d66g0115eck45c322601945c48cce2e120?)
---
+++ netty-learn/src/main/java/com/longlive/netty/NettyServer.java
@@ -0,0 +1,36 @@
+package com.longlive.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+
+/**
+ * Anything that can go wrong will go wrong
+ *
+ * @author Xingjian LONG <longxingjian@kuaishou.com>
+ * @date 2023-05-11
+ */
+public class NettyServer {
+    public static void main(String[] args) {
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+        NioEventLoopGroup boss = new NioEventLoopGroup();
+        NioEventLoopGroup worker = new NioEventLoopGroup();
+        serverBootstrap.group(boss, worker).channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    protected void initChannel(NioSocketChannel ch) {
+                        ch.pipeline().addLast(new StringDecoder());
+                        ch.pipeline().addLast(new SimpleChannelInboundHandler<String>() {
+                            @Override
+                            protected void channelRead0(ChannelHandlerContext ct, String msg) {
+                                System.out.println("[" + Thread.currentThread().getName() + "]" + msg);
+                            }
+                        });
+                    }
+                }).bind(8000);
+    }
+}
