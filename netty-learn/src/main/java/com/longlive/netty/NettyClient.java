2309dcd930b96b9126830b1723a080befc678623
longxingjian <longxingjian@kuaishou.com>
2023-05-16 14:42:58 +0800

    for now

--- netty-learn/src/main/java/com/longlive/netty/NettyClient.java
+++ netty-learn/src/main/java/com/longlive/netty/NettyClient.java
@@ -1,13 +1,16 @@
 package com.longlive.netty;
 
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.string.StringEncoder;
+import io.netty.util.AttributeKey;
 
 /**
  * Anything that can go wrong will go wrong
@@ -18,18 +21,49 @@ import io.netty.handler.codec.string.StringEncoder;
 public class NettyClient {
     public static void main(String[] args) throws InterruptedException {
         Bootstrap bootstrap = new Bootstrap();
+        //指定线程模型
         NioEventLoopGroup group = new NioEventLoopGroup();
-        bootstrap.group(group).channel(NioSocketChannel.class)
+        bootstrap.group(group)
+                // 指定IO模型
+                .channel(NioSocketChannel.class)
+                .attr(AttributeKey.newInstance("clientName"), "nettyClient")
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+                .option(ChannelOption.SO_KEEPALIVE, true)
+                .option(ChannelOption.TCP_NODELAY, true)
+                // IO处理逻辑
                 .handler(new ChannelInitializer<Channel>() {
                     @Override
                     protected void initChannel(Channel ch) {
                         ch.pipeline().addLast(new StringEncoder());
                     }
                 });
-        Channel channel = bootstrap.connect("127.0.0.1", 8000).channel();
+        Channel channel = connect(bootstrap, "127.0.0.1", 8000, MAX_RETRY);
+        // 建立连接
         while (true) {
             channel.writeAndFlush(new Date() + ": hello world!");
             Thread.sleep(2000);
         }
+    }
+
+
+    private static final int MAX_RETRY = 3;
+
+    // 指数退避客户端重连
+    private static Channel connect(final Bootstrap bootstrap, final String host, final int port, int retry) {
+        return bootstrap.connect(host, port).addListener(
+                future -> {
+                    if (future.isSuccess()) {
+                        System.out.println("连接成功!");
+                    } else if (retry == 0) {
+                        System.out.println("重试次数已用完，放弃连接!");
+                    } else {
+                        int order = MAX_RETRY - retry + 1;
+                        int delay = 1 << order;
+                        System.out.println(new Date() + ": 连接失败，第" + order + "次重连......");
+                        bootstrap.config().group()
+                                .schedule(() -> connect(bootstrap, host, port, retry - 1), delay, TimeUnit.SECONDS);
+                    }
+                }
+        ).channel();
     }
 }
--- netty-learn/src/main/java/com/longlive/netty/NettyServer.java
+++ netty-learn/src/main/java/com/longlive/netty/NettyServer.java
@@ -3,11 +3,16 @@ package com.longlive.netty;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.string.StringDecoder;
+import io.netty.util.Attribute;
+import io.netty.util.AttributeKey;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.GenericFutureListener;
 
 /**
  * Anything that can go wrong will go wrong
@@ -20,7 +25,16 @@ public class NettyServer {
         ServerBootstrap serverBootstrap = new ServerBootstrap();
         NioEventLoopGroup boss = new NioEventLoopGroup();
         NioEventLoopGroup worker = new NioEventLoopGroup();
-        serverBootstrap.group(boss, worker).channel(NioServerSocketChannel.class)
+        serverBootstrap.group(boss, worker)
+                .channel(NioServerSocketChannel.class)
+                // 用于指定在服务端启动过程中的逻辑
+                .handler(new ChannelInitializer<NioServerSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioServerSocketChannel nioServerSocketChannel) {
+                        System.out.println("服务启动中");
+                    }
+                })
+                // 用于指定处理新连接数据的读写处理逻辑
                 .childHandler(new ChannelInitializer<NioSocketChannel>() {
                     protected void initChannel(NioSocketChannel ch) {
                         ch.pipeline().addLast(new StringDecoder());
@@ -31,6 +45,32 @@ public class NettyServer {
                             }
                         });
                     }
-                }).bind(8000);
+                })
+                // 可以给服务端channel 指定一些自定义属性，然后可以通过channel.attr()取出属性
+                .attr(AttributeKey.newInstance("serverName"), "nettyServer")
+                // 可以给每一个连接都指定自定义属性
+                .childAttr(AttributeKey.newInstance("clientKey"), "clientValue")
+                // 可以给服务端channel 设置TCP参数，下面的设置表示系统用于临时存放已完成三次握手的请求的队列的最大长度
+                // 如果连接建立频繁，服务器处理创建新连接较慢，则可以适当调大这个参数
+                .option(ChannelOption.SO_BACKLOG, 1024)
+                // 可以给每个连接设置一些TCP参数
+                // TCP心跳
+                .childOption(ChannelOption.SO_KEEPALIVE, true)
+                // 开启Nagle算法
+                // Nagle 算法收集小包集中发送
+                .childOption(ChannelOption.TCP_NODELAY, true)
+        ;
+        bind(serverBootstrap, 8000);
+    }
+
+    private static void bind(final ServerBootstrap serverBootstrap, final int port) {
+        serverBootstrap.bind(port).addListener(future -> {
+            if (future.isSuccess()) {
+                System.out.println("端口[" + port + "]" + "绑定成功");
+            } else {
+                System.out.println("端口[" + port + "]" + "绑定失败");
+                bind(serverBootstrap, port + 1);
+            }
+        });
     }
 }
